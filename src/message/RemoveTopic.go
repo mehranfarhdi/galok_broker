d33a628eabfc99e51eb6d94a89f77f7754360184
mehran <mehran.farhadi@aut.ac.ir>
2024-01-02 09:57:44 +0330

    Add remove topic class

--- src/message/RemoveTopic.go
+++ src/message/RemoveTopic.go
@@ -1,1 +1,33 @@
 package message
+
+type RemoveTopic struct {
+	MessageType int      `json:"message_type"`
+	Topics      []string `json:"topics"`
+	Username    string   `json:"username"`
+	Password    string   `json:"password"`
+}
+
+func NewRemoveTopic(messagetype int, topics []string, username, password string) RemoveTopic {
+	return RemoveTopic{
+		MessageType: messagetype,
+		Topics:      topics,
+		Username:    username,
+		Password:    password,
+	}
+}
+
+func (m RemoveTopic) GetMessageType() int {
+	return m.MessageType
+}
+
+func (t RemoveTopic) GetTopics() []string {
+	return t.Topics
+}
+
+func (t RemoveTopic) GetUsername() string {
+	return t.Username
+}
+
+func (t RemoveTopic) GetPassword() string {
+	return t.Password
+}
